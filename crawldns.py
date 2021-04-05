ff42f08d50ecb8fd981504bdc125245ecadc2fe1
krishpranav <krisna.pranav@gmail.com>
2021-04-05 12:46:46 +0530

    done changes

--- crawldns.py
+++ crawldns.py
@@ -61,3 +61,38 @@ def GetSubdomains(cursor):
      for row in res.fetchall():
          print('Subdomain found: {0}'.format(row[0]))
 
+def GetLinks(CdxApi, IndexNum):
+    	print("Processing {0}".format(IndexNum))
+	Req = requests.get(CdxApi+'?url='+Target+'&fl=url&matchType=domain&pageSize=2000&output=json')
+	Ans = Req.text.split('\n')[:-1]
+	try:
+		for entry in Ans:
+			Url = json.loads(entry)['url']
+			if PrintURL is True and FileExt is not None:
+				URLZ = re.findall(FileExt, Url)
+				if URLZ:
+					print(Url)
+			if PrintURL is True and FileExt is None:
+				print(Url)
+
+			Domains =  re.findall(r'(?<=://)[^/|?|#]*', Url)[0]
+			SaveSubDomainToDb({
+					'Domain': Domains
+					})
+		print("{0} Processed".format(IndexNum))
+	except:
+		pass
+
+def GetIndexFile():
+	IndexURL = "https://index.commoncrawl.org/collinfo.json"
+	Data = requests.get(IndexURL).text
+	Indexes = json.loads(Data)
+	threads = []
+	print("You have %s CPUs...\nLet's use them all!"%multiprocessing.cpu_count())
+	Pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+	for res in Indexes:
+		proc = Pool.apply_async(GetLinks, (res['cdx-api'],(res['id'])))
+		threads.append(proc)
+	for proc in threads:
+		proc.get()
+
